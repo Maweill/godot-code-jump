aab65c95f8ae828afc10afdd819b479a900d7c99
Bednyakov <bednyakov99@gmail.com>
2024-02-25 21:16:51 +0300

    set caret position to first word which start from chosen letter

--- addons/code_jump/code_jump.gd
+++ addons/code_jump/code_jump.gd
@@ -8,6 +8,12 @@ const ACTIVATE_PLUGIN_SHORTCUT_SETTING_NAME: StringName = CODE_JUMP_SETTING_NAME
 var activate_plugin_shortcut: Shortcut
 #endregion
 
+var text_editor: TextEdit
+
+var listening_for_jump_letter: bool
+var listeting_for_navigation_letter: bool
+var jump_letter: String
+
 func _enter_tree() -> void:
 	var editor_settings: EditorSettings = get_editor_settings()
 	activate_plugin_shortcut = get_or_create_activate_plugin_shortcut(editor_settings)
@@ -20,25 +26,50 @@ func _unhandled_key_input(event: InputEvent) -> void:
 	if !(event is InputEventKey):
 		return
 
-	if (activate_plugin_shortcut.matches_event(event)):
-		var editor = EditorInterface.get_script_editor()
-		var text_editor = editor.get_current_editor().get_base_editor()
-		var first_visible_line_index = text_editor.get_first_visible_line()
-		var last_visible_line_index = text_editor.get_last_full_visible_line()
-		var visible_lines_text = ""
-		# спавним по курсору на каждое слово
-		# меняем первую букву на "a", "b", "c"...
-		# ждем ввода пользователя
-		# перемещаем курсор к выбранному слову
-		# возвращаем первые буквы в исходное
-		for line_index in range(first_visible_line_index, last_visible_line_index + 1):
-			visible_lines_text += text_editor.get_line(line_index)
-		var whole_words = get_words_starting_with_letter(visible_lines_text, "g")
-		print("whole_words=%s" % whole_words.is_empty())
-		var search_result = text_editor.search(whole_words[0], 2, first_visible_line_index, 0)
-		print("search_result=%s" % search_result)
-		text_editor.set_caret_line(search_result.y, false)
-		text_editor.set_caret_column(search_result.x, false)
+	if listeting_for_navigation_letter and event.is_released():
+		text_editor.grab_focus()
+		return
+
+	if !event.is_pressed():
+		return
+
+	var editor = EditorInterface.get_script_editor()
+	text_editor = editor.get_current_editor().get_base_editor()
+
+	if listening_for_jump_letter:
+		listening_for_jump_letter = false
+		listeting_for_navigation_letter = true
+
+		jump_letter = (event as InputEventKey).as_text_key_label()
+		print("jump_letter=%s" % jump_letter)
+		highlight_matches()
+		return
+
+	if activate_plugin_shortcut.matches_event(event):
+		listening_for_jump_letter = true
+		listeting_for_navigation_letter = false
+		print("listening for jump key")
+		text_editor.release_focus()
+		return
+
+func highlight_matches() -> void:
+	var first_visible_line_index = text_editor.get_first_visible_line()
+	var last_visible_line_index = text_editor.get_last_full_visible_line()
+	var visible_lines_text = ""
+
+	# спавним по курсору на каждое слово
+	# меняем первую букву на "a", "b", "c"...
+	# ждем ввода пользователя
+	# перемещаем курсор к выбранному слову
+	# возвращаем первые буквы в исходное
+	for line_index in range(first_visible_line_index, last_visible_line_index + 1):
+		visible_lines_text += text_editor.get_line(line_index)
+	var whole_words = get_words_starting_with_letter(visible_lines_text, jump_letter)
+	print("whole_words=%s" % whole_words.is_empty())
+	var search_result = text_editor.search(whole_words[0], 2, first_visible_line_index, 0)
+	print("search_result=%s" % search_result)
+	text_editor.set_caret_line(search_result.y, false)
+	text_editor.set_caret_column(search_result.x, false)
 
 func get_words_starting_with_letter(text: String, letter: String) -> Array[String]:
 	# Regular expression to split the text by non-word characters
@@ -52,7 +83,6 @@ func get_words_starting_with_letter(text: String, letter: String) -> Array[Strin
 	var filtered_words: Array[String] = []
 	for word in words:
 		var word_string = word.get_string()
-		print("WORD_STRING=%s" % word_string)
 		if word_string.begins_with(letter.to_lower()) or word_string.begins_with(letter.capitalize()): # Case-insensitive check
 			filtered_words.append(word_string)
 
