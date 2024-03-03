eda1d6aa15a6aef37365168b06d27a3fb8a802e5
Bednyakov <bednyakov99@gmail.com>
2024-03-03 13:23:58 +0300

    define plugin states

---
+++ addons/code_jump/states/common/state.gd
@@ -0,0 +1,10 @@
+class_name CJState
+
+func on_enter() -> void:
+	pass
+
+func on_exit() -> void:
+	pass
+
+func on_input(event: InputEvent) -> void:
+	pass
---
+++ addons/code_jump/states/listen_hint_letter_state.gd
@@ -0,0 +1,11 @@
+class_name CJListenHintLetterState
+extends CJState
+
+func on_enter() -> void:
+	pass
+
+func on_exit() -> void:
+	pass
+
+func on_input(event: InputEvent) -> void:
+	pass
